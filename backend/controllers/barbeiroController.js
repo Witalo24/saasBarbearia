0d8c35bf5fcb43aa4b515f6a1e20f11753863ec5
Witalo24 <witalops2004@gmail.com>
2025-11-14 14:58:45 -0300

    Adicionei controllers de barbeiros

--- backend/controllers/barbeiroController.js
+++ backend/controllers/barbeiroController.js
@@ -1,31 +1,36 @@
 const db = require("../config/db");
 
-exports.listar = async (req, res) => {
-    try{
-        const [rows] = await db.query("SELECT * FROM barbeiros WHERE barbearia_id = ?", [req.barbeariaId]);
-        res.json(rows);
+exports.criarBarbeiro = async (req, res) => {
+    try {
+        const { nome, email, barbearia_id, telefone } = req.body;
+
+        if (!barbearia_id || !nome || !email) {
+            return res.status(400).json({ erro: "Campos obrigatorios não informados" });
+        }
+
+        const [existe] = await db.query("SELECT * FROM barbeiros WHERE email = ?", [email]);
+
+        if (existe.length > 0) return res.status(400).json({ erro: "E-mail já cadastrado" });
+
+        await db.query("INSERT INTO barbeiros (nome, email, telefone, barbearia_id", [nome, email, telefone, barbearia_id]);
+
+        res.json({ mensagem: "Barbeiro cadastrado com sucesso!" })
+
     } catch (erro) {
         console.error(erro);
-        res.status(500).json({erro: "Erro ao listar barbeiros"})
+        res.status(500).json({ erro: "Erro ao cadastrar barbeiro" })
     }
 };
-exports.criar = async (req, res) => {
-    try{
-        const {nome, especialidade} = req.body;
-        await db.query("INSERT INTO barbeiros (nome, especialidade, barbearia_id) VALUES (?, ?, ?)", [nome, especialidade, req.barbeariaId])
-        res.json({mensagem: "Barbeiro cadastrado com sucesso"});
-    } catch (erro) {
-        console.error(erro);
-        res.status(500).json({erro: "Erro ao cadastrar barbeiro"});
-    }
-};
-exports.excluir = async (req, res) => {
-    try{
-        const {id} = req.params;
-        await db.query("DELETE FROM barbeiros WHERE id = ? AND barbearia_id = ? ", [id, req.barbeariaId]);
-        res.json({mensagem: "Barbeiro excluido"});
-    } catch (erro) {
-        console.error(erro)
-        res.staus(500).json({erro: "Erro ao excluir barbeiro"});
-    }
-};
+
+exports.listarBarbeiros = async (req, res) => {
+    try {
+        const { barbearia_id } = req.params;
+
+        if(!barbearia_id) return res.status(400).json({erro: "ID da barbearia é obrigatorio"});
+
+        const [rows] = await db.query("SELECT * FROM barbeiros WHERE barbearia_id = ?", [barbearia_id]);
+        res.json({rows})
+    } catch (erro) {}
+   
+
+}
